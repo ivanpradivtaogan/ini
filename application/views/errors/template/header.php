1f9d7640c4e1fce0adffb1e070961b876f0e63b9
ivanpradivtaogan <ivanpradivta@gmail.com>
2022-06-20 21:05:27 +0700

    ini

---
+++ application/views/errors/template/header.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+<head>
+<meta http-equiv="content-type" content="text/html; charset=iso-8859-1"/>
+<link rel="stylesheet" type="text/css" href="<?php echo base_url()?>assets/default.css" media="screen"/>
+<title>The Hobbit</title>
+</head>
+<body>
+<div class="container">
+  <div class="gfx"><span></span></div>
+  <div class="top">
+    <div class="navigation"> <a href="#" id="selected">home</a> <a href="#">the journey</a> <a href="#">characters</a> <a href="#">image gallery</a> <a href="#">history</a> </div>
+    

b2263b25c376f5b025cc44e97525ae2ad0320d3c
ivanpradivtaogan <ivanpradivta@gmail.com>
2022-06-20 21:29:22 +0700

    buat header

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -4,9 +4,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
 
 public function index(){
-    $this->load->view('home/index');
-    $this->load->view('template/footer');
     $this->load->view('template/header');
+    $this->load->view('home/index');
+    
 
 }
 }
